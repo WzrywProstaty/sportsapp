d1d967bb9dc5a66b5bad9f14b4b1cbd84c355526
Илья Страхов <strakhov.ilya@lab-company.ru>
2024-04-24 13:39:44 +0300

    renamed the WorkoutActions component

--- src/Shared/ui-kit/index.ts
+++ src/Shared/ui-kit/index.ts
@@ -1,10 +1,10 @@
 import {Button} from './buttons/Button/Button.tsx'
-import {EditActionWorkouts} from "./buttons/EditActionWorkouts/EditActionWorkouts.tsx";
+import {WorkoutsActions} from "./buttons/WorkoutsActions/WorkoutsActions.tsx";
 
 import Spinner01 from "./spinners/spinner01/spinner01.tsx";
 
 export {
     Button as Button,
     Spinner01 as Spinner01,
-    EditActionWorkouts as EditActionWorkouts
+    WorkoutsActions as WorkoutsActions
 }

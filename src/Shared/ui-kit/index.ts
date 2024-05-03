181d99c0f5842a0d64a0129969e3445117e700f5
Илья Страхов <strakhov.ilya@lab-company.ru>
2024-05-03 10:36:17 +0300

    added dependencies, refactored styles

--- src/Shared/ui-kit/index.ts
+++ src/Shared/ui-kit/index.ts
@@ -1,6 +1,7 @@
-import Button from './buttons/Button/Button.tsx'
-import {WorkoutsActions} from "./buttons/WorkoutsActions/WorkoutsActions.tsx";
-import DoubleSwitcher from './buttons/DoubleSwitcher/DoubleSwitcher.tsx';
+import Button from '~Shared/ui-kit/Components/Button/Button.tsx'
+import {WorkoutsActions} from "~Shared/ui-kit/Components/WorkoutsActions/WorkoutsActions.tsx";
+import DoubleSwitcher from '~Shared/ui-kit/Components/DoubleSwitcher/DoubleSwitcher.tsx';
+import ContainerNamed from "~Shared/ui-kit/Components/ContainerNamed/ContainerNamed.tsx";
 
 import Spinner01 from "./spinners/spinner01/spinner01.tsx";
 
@@ -8,5 +9,6 @@ export {
     Button as Button,
     Spinner01 as Spinner01,
     WorkoutsActions as WorkoutsActions,
-    DoubleSwitcher as DoubleSwitcher
+    DoubleSwitcher as DoubleSwitcher,
+    ContainerNamed as ContainerNamed
 }

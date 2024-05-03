2d9556b219a9f320692de950ae7fe8d5214234bd
Илья Страхов <strakhov.ilya@lab-company.ru>
2024-05-03 10:35:11 +0300

    Created named container component

---
+++ src/Shared/ui-kit/Components/ContainerNamed/ContainerNamed.tsx
@@ -0,0 +1,60 @@
+import styles from './ContainerNamed.module.scss'
+import PropsContainerNamed from "~Shared/ui-kit/Components/ContainerNamed/ContainerNamedTypes.ts";
+import classNames from "classnames";
+const ContainerNamed: React.FC<PropsContainerNamed> = ({
+    edited = false,
+    removed = false,
+    isDropdown = true,
+    headTitle= 'Container name',
+    contentTemplate,
+    cellularTemplate,
+    isCollapsed = false,
+    ...rest
+                        }) => {
+
+    const headClasses = classNames(
+        styles.cb_head,
+        {[styles.cb_head__collapsed] : isCollapsed},
+        {[styles.cb_head__opened] : !isCollapsed},
+        {[styles.cb_head__black] : !isCollapsed},
+    );
+    const contentWrapperClasses = classNames(
+        styles.cb_content_wrapper,
+        {[styles.cb_content_wrapper__collapsed] : isCollapsed}
+    );
+    const contentClasses = classNames(
+        styles.cb_content__shadow,
+        styles.cb_content
+    );
+    const headTextareaClasses = classNames(
+        styles.cb_head_textarea,
+        {[styles.cb_head_textarea__readonly] : removed || isDropdown},
+        {[styles.cb_head_textarea__edited] : edited},
+        {[styles.cb_head_textarea__removed] : removed},
+    );
+    const cellularClasses = classNames(
+        styles.cb_cellular,
+    );
+
+    return(
+        <div {...rest} className={styles.cb__dropdown}>
+            <div className={headClasses}>
+                <textarea value={headTitle} rows={1} readOnly={!edited || removed} className={headTextareaClasses}></textarea>
+            </div>
+
+            <div className={contentWrapperClasses}>
+                <div>
+                    <div className={contentClasses}>{contentTemplate}</div>
+                    {cellularTemplate &&
+                        <div className={cellularClasses}>{cellularTemplate}</div>
+                    }
+                </div>
+
+            </div>
+
+
+        </div>
+    );
+}
+
+export default ContainerNamed
---
+++ src/Shared/ui-kit/Components/ContainerNamed/ContainerNamedTypes.ts
@@ -0,0 +1,10 @@
+export default interface PropsContainerNamed extends React.HTMLAttributes<HTMLDivElement>{
+    isDropdown?: boolean | undefined;
+    isCollapsed?: boolean | undefined;
+    headTitle?: string | undefined;
+    contentTemplate: React.ReactElement;
+    cellularTemplate?: React.ReactElement | undefined;
+    edited?: boolean | undefined;
+    removed?: boolean | undefined;
+    readonly?: boolean | undefined;
+}

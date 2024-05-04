705d68747407ef5e7bb6c6c99f32a7af39bdfd37
Илья Страхов <strakhov.ilya@lab-company.ru>
2024-05-04 16:26:42 +0300

    made a simple container without any hard logics

--- src/Shared/ui-kit/index.ts
+++ src/Shared/ui-kit/index.ts
@@ -2,6 +2,7 @@ import Button from '~Shared/ui-kit/Components/Button/Button.tsx'
 import {WorkoutsActions} from "~Shared/ui-kit/Components/WorkoutsActions/WorkoutsActions.tsx";
 import DoubleSwitcher from '~Shared/ui-kit/Components/DoubleSwitcher/DoubleSwitcher.tsx';
 import ContainerNamed from "~Shared/ui-kit/Components/ContainerNamed/ContainerNamed.tsx";
+import ContainerSimple from "~Shared/ui-kit/Components/ContainerSimple/ContainerSimple.tsx";
 
 import Spinner01 from "./spinners/spinner01/spinner01.tsx";
 
@@ -10,5 +11,6 @@ export {
     Spinner01 as Spinner01,
     WorkoutsActions as WorkoutsActions,
     DoubleSwitcher as DoubleSwitcher,
-    ContainerNamed as ContainerNamed
+    ContainerNamed as ContainerNamed,
+    ContainerSimple as ContainerSimple,
 }

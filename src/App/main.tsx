9adfae003a37eb37df399720b7a6e5761658a067
Илья Страхов <strakhov.ilya@lab-company.ru>
2024-05-07 10:30:23 +0300

    Main style changes

--- src/App/main.tsx
+++ src/App/main.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-import '~Shared/ui-kit/font-families/Glory/Glory_font_face.css'
-import 'reset-css'
-import '~Shared/utility-styles/font-variants.scss'
+
 import '~App/assets/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
